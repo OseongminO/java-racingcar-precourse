254dd3ee553357bdc80f513a895fc40ce7742364
seongmin <qowlxj08@naver.com>
2021-05-01 14:14:56 +0900

    add 자동차 게임 차량정보 입력 및 validation

---
+++ src/main/java/cargame/InputCarInfo.java
@@ -0,0 +1,59 @@
+package cargame;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class InputCarInfo {
+    private final Scanner scan = new Scanner(System.in);
+    private List<String> carList;
+
+    public List<String> inputCar() {
+        while (true) {
+            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+            String carInfo = scan.nextLine();
+            this.carList = Arrays.asList(carInfo.split(","));
+            try {
+                validation(this.carList);
+                break;
+            } catch (RuntimeException e) {
+            }
+        }
+        return this.carList;
+    }
+
+    public void validation(List<String> carList) {
+        if (carList.size() <= 1) {
+            System.out.println("경주 차량이 부족합니다.");
+            throw new IllegalArgumentException("경주 차량이 부족합니다.");
+        }
+        for(int i=0; i < carList.size(); i++){
+            checkCarName(carList.get(i));
+        }
+        carDoubleCheck(carList);
+    }
+
+    private void carDoubleCheck(List<String> carList) {
+        for(int i =0; i < carList.size();i++){
+            String checkCar = carList.get(i);
+            carList.set(i,"");
+            doubleCheck(carList,checkCar,i);
+        }
+    }
+
+    private void doubleCheck(List<String> carList, String checkCar, int i) {
+        if(carList.contains(checkCar)){
+            System.out.println("중복된 차량이 존재합니다.");
+            throw new IllegalArgumentException("중복된 차량이 존재합니다.");
+        }
+        carList.set(i,checkCar);
+    }
+
+    private void checkCarName(String s) {
+        if(s.length() > 5){
+            System.out.println("자동차 이름은 5자 이내로 입력해주세요.");
+            throw new IllegalArgumentException("자동차 이름은 5자 이내로 입력해주세요.");
+        }
+    }
+}
